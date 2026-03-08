ccb161ce4d1ad31eeaba03bbb5dfba2f4bb3dfb3
SudarshanRudrapure <sudarshanrudrapure@gmail.com>
2026-03-08 23:24:31 +0530

    Counter-App-Project

--- counter-app/counter.js
+++ counter-app/counter.js
@@ -1,21 +1,37 @@
 let count = 0;
 
-const countValue = document.getElementById("count");
+const countElement = document.getElementById("count");
 const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
 const resetBtn = document.getElementById("reset");
 
-increaseBtn.addEventListener("click", function () {
+function updateCounter() {
+
+  countElement.textContent = count;
+
+  if(count > 0){
+    countElement.style.color = "green";
+  }
+  else if(count < 0){
+    countElement.style.color = "red";
+  }
+  else{
+    countElement.style.color = "black";
+  }
+
+}
+
+increaseBtn.addEventListener("click", function(){
   count++;
-  countValue.textContent = count;
+  updateCounter();
 });
 
-decreaseBtn.addEventListener("click", function () {
+decreaseBtn.addEventListener("click", function(){
   count--;
-  countValue.textContent = count;
+  updateCounter();
 });
 
-resetBtn.addEventListener("click", function () {
+resetBtn.addEventListener("click", function(){
   count = 0;
-  countValue.textContent = count;
+  updateCounter();
 });
